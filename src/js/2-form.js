2cb45f696cc035887a63e75aa66a40b55aa1320d
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-03-20 17:01:31 +0300

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,42 +1,40 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('.feedback-form');
+const form = document.querySelector('.feedback-form');
 
-    
-    const savedFormData = JSON.parse(localStorage.getItem("feedback-form-state"));
-    if (savedFormData) {
-        form.elements.email.value = savedFormData.email || '';
-        form.elements.message.value = savedFormData.message || '';
-    }
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
 
-    
-    form.addEventListener('input', function () {
-        const email = form.elements.email.value.trim();
-        const message = form.elements.message.value.trim();
-        const formData = {
-            email: email,
-            message: message
-        };
-        localStorage.setItem("feedback-form-state", JSON.stringify(formData));
-    });
+    const email = form.elements.email.value.trim();
+    const message = form.elements.message.value.trim();
+    const formData = {
+        email: email,
+        message: message
+    };
+
+
+    localStorage.removeItem("feedback-form-state");
 
    
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
+    form.reset();
 
-        const email = form.elements.email.value.trim();
-        const message = form.elements.message.value.trim();
-        const formData = {
-            email: email,
-            message: message
-        };
+    
+    console.log(formData);
+});
 
-       
-        localStorage.removeItem("feedback-form-state");
 
-       
-        form.reset();
+const savedFormData = JSON.parse(localStorage.getItem("feedback-form-state"));
+if (savedFormData) {
+    form.elements.email.value = savedFormData.email || '';
+    form.elements.message.value = savedFormData.message || '';
+}
 
-        
-        console.log(formData);
-    });
+
+form.addEventListener('input', function () {
+    const email = form.elements.email.value.trim();
+    const message = form.elements.message.value.trim();
+    const formData = {
+        email: email,
+        message: message
+    };
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
 });
