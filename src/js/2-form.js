25e926ec7e6809feefcb8e5be1d8cbd8b94df330
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-03-20 16:20:50 +0300

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,3 +1,42 @@
-import SimpleLightbox from "simplelightbox";
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('.feedback-form');
 
-import "simplelightbox/dist/simple-lightbox.min.css";
+    
+    const savedFormData = JSON.parse(localStorage.getItem("feedback-form-state"));
+    if (savedFormData) {
+        form.elements.email.value = savedFormData.email || '';
+        form.elements.message.value = savedFormData.message || '';
+    }
+
+    
+    form.addEventListener('input', function () {
+        const email = form.elements.email.value.trim();
+        const message = form.elements.message.value.trim();
+        const formData = {
+            email: email,
+            message: message
+        };
+        localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+    });
+
+   
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const email = form.elements.email.value.trim();
+        const message = form.elements.message.value.trim();
+        const formData = {
+            email: email,
+            message: message
+        };
+
+       
+        localStorage.removeItem("feedback-form-state");
+
+       
+        form.reset();
+
+        
+        console.log(formData);
+    });
+});
